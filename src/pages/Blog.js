63f4bd2353a90ea6ebb45596bd991c495c681d4b
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2022-01-12 21:20:28 +0200

    useSearchParams

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,8 +1,16 @@
 import {useEffect, useState} from "react";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
+import BlogFilter from "../components/BlogFilter";
 
 export default function Blog () {
     const [posts, setPosts] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const postQuery = searchParams.get('post') || '';
+    // example: URL.ru/posts?post=abc&data=qwe ...
+    const latest = searchParams.has('latest');
+
+    const startsFrom = latest ? 80 : 1;
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -13,9 +21,12 @@ export default function Blog () {
 return (
 <div>
     <h1>Our Blog</h1>
+    <BlogFilter postQuery={postQuery} latest={latest} setSearchParams={setSearchParams}/>
     <Link to={'/posts/new'}>Add new post</Link>
     {
-        posts.map(value => (
+        posts.filter(
+            post => post.title.includes(postQuery) && post.id >= startsFrom
+        ).map(value => (
             <Link key={value.id} to={`/posts/${value.id}`}>
                 <li>{value.title}</li>
             </Link>
